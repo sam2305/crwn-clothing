576450674bb40d8d410ea176ffb1fbd46b895911
Sam <smath045@fiu.edu>
2021-03-19 00:58:47 -0400

    Add Backend Express server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,11 +1,26 @@
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
 const StripeCheckoutButton = ({price})=> {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51ITvPvCtzG3N1WzK7Xv4meARIoaN5C8lxzSpgJyzoaMaj0PB8qWU5udmbqBRtTI9wYhJQHj7if8im3GWVYTOaiBR00iNseCEku';
     const onToken= token=> {
-         alert('Payment Successful');
+        axios({
+           url:'payment',
+           method:'post',
+           data:{
+               amount: priceForStripe,
+               token
+           }
+        }).then(response => {
+            console.log(response);
+            alert('Payment Successful');
+        }).catch(error=>{
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please use the provided credit card.');
+        })
+         
     }
     return (
         <StripeCheckout
